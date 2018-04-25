8ece019e2fda68ff4a0ac9dc229d611e6fdc927e
Alkesst <alec.augal@gmail.com>
2018-04-25 14:21:49 +0200

    Initial Commit

---
+++ Copinya/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Copinya
+//
+//  Created by Alejandro Garau Madrigal on 25/04/2018.
+//  Copyright © 2018 Alejandro Garau Madrigal. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
